6dbbe6c8a15e51c46a7762f8216bdc19eae0161c
SeBin-Kwon <sbkwon98@gmail.com>
2023-06-02 16:52:25 +0900

    Initial Commit

---
+++ TodoBiny/TodoBinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoBinyApp.swift
+//  TodoBiny
+//
+//  Created by Sebin Kwon on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct TodoBinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
